d51fd0762f9f981542e95cca3aa3d2ae5346348f
tuhinOfficial <tuhin.official100@gmail.com>
2023-05-18 02:59:58 +0600

    complete Home Section Design

--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
 https://i.ibb.co/jzdbXZJ/cooltext435753034134748.png
 https://i.ibb.co/KzLCdPV/8100-2-02.jpg
+https://i.ibb.co/6y4tysb/fluffy-toy-texture-close-up.jpg
+https://i.ibb.co/vBdDT7G/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-ta.jpg
+https://i.ibb.co/jz78mLY/handsome-confident-smiling-man-with-hands-crossed-chest.jpg
+https://i.ibb.co/6cCxzVW/good-deal-right-corner-confident-pleasant-friendly-looking-african-american-gorgeous-woman-with-afro.jpg
+https://i.ibb.co/VwYLp3b/cheerful-curly-business-girl-wearing-glasses.jpg
--- src/components/Pages/Home/Home/Home.jsx
+++ src/components/Pages/Home/Home/Home.jsx
@@ -1,7 +1,13 @@
 import { AiOutlineArrowRight } from "react-icons/ai";
-import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import { Player} from "@lottiefiles/react-lottie-player";
+import "./Home.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+
+    AOS.init();
+
   return (
     <div className="mt-16">
       <section className="banner">
@@ -30,6 +36,91 @@ const Home = () => {
               style={{ height: "300px", width: "300px" }}
             ></Player>
           </div>
+        </div>
+      </section>
+
+      <section className="trending my-20">
+        <div>
+          <h2 className="text-3xl font-semibold text-center">Trending Toys</h2>
+        </div>
+      </section>
+
+      <section className="my-20">
+        <div className="client">
+          <div className="flex justify-evenly py-20 text-white font-bold text-center">
+            <div>
+              <h1 className="text-3xl">Products</h1>
+              <span className="text-2xl">5000 +</span>
+            </div>
+            <div>
+              <h1 className="text-3xl">Customers</h1>
+              <span className="text-2xl">50k +</span>
+            </div>
+            <div>
+              <h1 className="text-3xl">Happy Customer</h1>
+              <span className="text-2xl">100K +</span>
+            </div>
+            <div>
+              <h1 className="text-3xl">Sales</h1>
+              <span className="text-2xl">$100K +</span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="review my-20">
+        <h1 className="text-3xl font-semibold text-center mb-16">
+          Customer Review
+        </h1>
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-8 ml-16">
+          <div data-aos="fade-down-right" data-aos-duration="1000" data-aos-offset="100" className="card w-96 bg-primary text-primary-content text-center">
+            <div className="card-body">
+              <div className="avatar">
+                <div className="w-14 rounded-full mx-auto">
+                  <img src="https://i.ibb.co/VwYLp3b/cheerful-curly-business-girl-wearing-glasses.jpg" />
+                </div>
+              </div>
+              <h2>Name1</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div data-aos="fade-left" data-aos-duration="1000" data-aos-offset="100" className="card w-96 bg-primary text-primary-content text-center">
+            <div className="card-body">
+              <div className="avatar">
+                <div className="w-14 rounded-full mx-auto">
+                  <img src="https://i.ibb.co/VwYLp3b/cheerful-curly-business-girl-wearing-glasses.jpg" />
+                </div>
+              </div>
+              <h2>Name2</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div data-aos="zoom-in-right" data-aos-duration="1000" data-aos-offset="100" className="card w-96 bg-primary text-primary-content text-center">
+            <div className="card-body">
+              <div className="avatar">
+                <div className="w-14 rounded-full mx-auto">
+                  <img src="https://i.ibb.co/VwYLp3b/cheerful-curly-business-girl-wearing-glasses.jpg" />
+                </div>
+              </div>
+              <h2>Name3</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          <div data-aos="zoom-in-left" data-aos-duration="1000" data-aos-offset="100" className="card w-96 bg-primary text-primary-content text-center">
+            <div className="card-body">
+              <div className="avatar">
+                <div className="w-14 rounded-full mx-auto">
+                  <img src="https://i.ibb.co/VwYLp3b/cheerful-curly-business-girl-wearing-glasses.jpg" />
+                </div>
+              </div>
+              <h2>Name4</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+          
+          
+         
         </div>
       </section>
     </div>
